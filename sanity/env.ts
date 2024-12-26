fe876030b96d39f70d55a7be7a0430c3179d1b43
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-12-26 22:52:07 +0500

    all ok

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,12 +2,14 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-12-24'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  "production",
+  // process.env.NEXT_PUBLIC_SANITY_DATASET,
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "tsi14ipt",
+  // process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
